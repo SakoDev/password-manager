45ebc2fc1622d0ce9d99733bbd8efeb0bb1a195c
yassine achehboune <44009319+SakoDev@users.noreply.github.com>
2024-03-21 23:26:47 +0800

    modified:   resources/views/livewire/account/account-create.blade.php

--- resources/views/livewire/account/account-create.blade.php
+++ resources/views/livewire/account/account-create.blade.php
@@ -8,6 +8,9 @@
                     <input wire:model.lazy='email' type="email" id="email"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="your email here" />
+                        @error('email')
+                            <p class="text-sm text-red-500">{{ $message }}</p>
+                        @enderror
                 </div>
                 <div>
                     <label for="password"
