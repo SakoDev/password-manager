da6060a07386bd5233e420a5cf3118eaffae97dd
yassine achehboune <44009319+SakoDev@users.noreply.github.com>
2024-03-21 23:23:19 +0800

    modified:   app/Livewire/Account/AccountCreate.php 	modified:   resources/views/livewire/account/account-create.blade.php

--- app/Livewire/Account/AccountCreate.php
+++ app/Livewire/Account/AccountCreate.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class AccountCreate extends Component
 {
+    public $site,$password,$email,$phone;
+    public function saveAccount(){
+        $this->validate([
+            'email'=>'required|email',
+            'password'=>'required|string',
+            'site'=>'string',
+            'phone'=>'numeric',
+        ]);
+    }
     public function render()
     {
         return view('livewire.account.account-create');
--- resources/views/livewire/account/account-create.blade.php
+++ resources/views/livewire/account/account-create.blade.php
@@ -1,31 +1,31 @@
 <div>
     <div class="max-w-lg p-6 mx-auto overflow-hidden bg-white my-7 rounded-xl md:max-w-3xl">
-        <form>
+        <form wire:submit.prevent='saveAccount'>
             <div class="grid gap-6 mb-6 md:grid-cols-2">
                 <div>
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email
                     </label>
-                    <input type="email" id="email"
+                    <input wire:model.lazy='email' type="email" id="email"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="your email here" />
                 </div>
                 <div>
                     <label for="password"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                    <input type="password" id="password"
+                    <input wire:model.lazy='password' type="password" id="password"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="•••••••••" />
                 </div>
                 <div>
                     <label for="Site"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Site web</label>
-                    <input type="text" id="site"
+                    <input wire:model.lazy='site' type="text" id="site"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Google gmail" />
                 </div>
                 <div>
                     <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone number</label>
-                    <input type="tel" id="phone"
+                    <input wire:model.lazy='phone' type="tel" id="phone"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="0646567463" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" />
                 </div>
